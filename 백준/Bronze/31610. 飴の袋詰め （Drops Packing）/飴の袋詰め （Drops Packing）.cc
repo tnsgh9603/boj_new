3682537b2b2cfd9a5fd69b54082289b694da01e6
Soon Ho Kwon <74086829+tnsgh9603@users.noreply.github.com>
2024-03-17 10:58:27 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main() {
+    int A, B, C;
+    cin >> A >> B >> C;
+    cout << (A * B + C);
+    return 0;
+}
