bd84d773be6c691c03171c538f68e717d09f3831
Soon Ho Kwon <74086829+tnsgh9603@users.noreply.github.com>
2024-04-05 10:35:11 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main() {
+    fastio;
+    int n;
+    cin >> n;
+    cout << (n & 1 ? "evoLmoCicS" : "SciComLove");
+    return 0;
+}
