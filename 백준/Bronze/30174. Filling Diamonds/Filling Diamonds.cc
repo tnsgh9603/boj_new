890b07b03a36bc2b74f8f425c7eb7e7012836cdb
Soon Ho Kwon <74086829+tnsgh9603@users.noreply.github.com>
2024-04-11 09:04:02 +0900

    [Bronze II] Title: Filling Diamonds, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/30174. Filling Diamonds/Filling Diamonds.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main() {
+    fastio;
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        cin >> n;
+        cout << n << '\n';
+    }
+    return 0;
+}
