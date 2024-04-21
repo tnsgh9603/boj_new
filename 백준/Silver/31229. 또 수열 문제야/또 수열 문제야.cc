f64457d45352db8e6934ceca4e7213fe15e759b1
Soon Ho Kwon <74086829+tnsgh9603@users.noreply.github.com>
2024-04-21 16:08:56 +0900

    [Silver V] Title: 또 수열 문제야, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/31229. 또 수열 문제야/또 수열 문제야.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+int main() {
+    fastio;
+    int N, A = -1;
+    cin >> N;
+    while (N--) {
+        A += 2;
+        cout << A << " ";
+    }
+    return 0;
+}
