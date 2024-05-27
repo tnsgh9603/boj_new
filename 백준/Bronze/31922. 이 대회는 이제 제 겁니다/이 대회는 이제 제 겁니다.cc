42f0dd19f663627222cce1e309ce500ad38f6fa2
Soon Ho Kwon <74086829+tnsgh9603@users.noreply.github.com>
2024-05-27 11:21:55 +0900

    [Bronze V] Title: 이 대회는 이제 제 겁니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main() {
+    fastio;
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << max(a + c, b);
+    return 0;
+}
